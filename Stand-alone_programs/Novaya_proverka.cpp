15e1c502112591dd075ed61a927151a07b9bd450
AndreySkurikhin1990 <74450761+AndreySkurikhin1990@users.noreply.github.com>
2023-02-04 22:06:36 +0300

    Rename Novaya_proverka.cpp to Stand-alone_programs/Novaya_proverka.cpp

--- Stand-alone_programs/Novaya_proverka.cpp
+++ Stand-alone_programs/Novaya_proverka.cpp
@@ -8,17 +8,17 @@
 #include <math.h>
 #include <windows.h>
 #include <iostream>
-#define vmivmf 1 //выбор метода измерений для фракции 2-0,7 мм: 0 - нестационарный, 1 - стационарный
-#define vyfv 1 //выбор фракции: 0 - фракция 2-0,7 мм, 1 - фракция 8-4 мм, 2 - фракция 1,6-0,35 мм
-#define vyuv 1 //выбор укладки: 1 - плоскопараллельная, 2 - вертикальная
-#define vysv 0 //выбор состояния: 0 - исходное, 1 - после повторных измерений, 2 - после прокаливания при 1000 град С
+#define vmivmf 1 //ГўГ»ГЎГ®Г° Г¬ГҐГІГ®Г¤Г  ГЁГ§Г¬ГҐГ°ГҐГ­ГЁГ© Г¤Г«Гї ГґГ°Г ГЄГ¶ГЁГЁ 2-0,7 Г¬Г¬: 0 - Г­ГҐГ±ГІГ Г¶ГЁГ®Г­Г Г°Г­Г»Г©, 1 - Г±ГІГ Г¶ГЁГ®Г­Г Г°Г­Г»Г©
+#define vyfv 1 //ГўГ»ГЎГ®Г° ГґГ°Г ГЄГ¶ГЁГЁ: 0 - ГґГ°Г ГЄГ¶ГЁГї 2-0,7 Г¬Г¬, 1 - ГґГ°Г ГЄГ¶ГЁГї 8-4 Г¬Г¬, 2 - ГґГ°Г ГЄГ¶ГЁГї 1,6-0,35 Г¬Г¬
+#define vyuv 1 //ГўГ»ГЎГ®Г° ГіГЄГ«Г Г¤ГЄГЁ: 1 - ГЇГ«Г®Г±ГЄГ®ГЇГ Г°Г Г«Г«ГҐГ«ГјГ­Г Гї, 2 - ГўГҐГ°ГІГЁГЄГ Г«ГјГ­Г Гї
+#define vysv 0 //ГўГ»ГЎГ®Г° Г±Г®Г±ГІГ®ГїГ­ГЁГї: 0 - ГЁГ±ГµГ®Г¤Г­Г®ГҐ, 1 - ГЇГ®Г±Г«ГҐ ГЇГ®ГўГІГ®Г°Г­Г»Гµ ГЁГ§Г¬ГҐГ°ГҐГ­ГЁГ©, 2 - ГЇГ®Г±Г«ГҐ ГЇГ°Г®ГЄГ Г«ГЁГўГ Г­ГЁГї ГЇГ°ГЁ 1000 ГЈГ°Г Г¤ Г‘
 using namespace std;
 const double pi = acos(-1e0), enu=1e-3, tocrasov=1e-4, tnoscv = 3e2, tnacv = 2e2, dtoscv = 1e2;
 const double tev0=273.15, tna=2e2+tev0, dtv=1e2, pksvv = 0.0;
 const double ssiv84 = 467.0*1e-3, salv84 = 129.0*1e-3, smgv84 = 282.0*1e-3;
 const double ssiv207 = 458.0*1e-3, salv207 = 14.0*1e-2, smgv207 = 29.0*1e-2;
 const int dsov=60, dmkooscv=14, cemv=12, nxtv=30, nnxtv=0;
-const int vtvn = 0, isrp=0, vpkf=0, vpmf=0; //0 - старые, 1 - новые
+const int vtvn = 0, isrp=0, vpkf=0, vpmf=0; //0 - Г±ГІГ Г°Г»ГҐ, 1 - Г­Г®ГўГ»ГҐ
 const double por207 = 66.35*1e-2, poris84 = 55.75*1e-2, porin84=81.53*1e-2;
 const double poro84=86.61*1e-2, poro16035=84.36*1e-2, por16035=83.97*1e-2;
 const char snmv[]="No memory";
@@ -60,17 +60,17 @@ int napMasEKTPshaNac();
 int main()
 { int k=0; //int j, jk = nxtv; double smgv=smgv84, ssiv=ssiv84, salv=salv84; initarrver(jk, smgv, ssiv - pksvv, salv + pksvv); //k=napMasEKTPVerNac();
 k=napMasEKTPshaNac(); //k=napMasEKTPkviNac(); //k=napMasEKTPitomNac();
-k=getchar();//int dmi=0; double **mu=rasPorpoRazVer(porver, vyfv, k, vysv, isrp, vpkf), *po; //0 - старые, 1 - новые значения //for (k=0; k<6; k++) { po=mu[k]; if (po) delete[]po; } if (mu) delete[]mu; 
+k=getchar();//int dmi=0; double **mu=rasPorpoRazVer(porver, vyfv, k, vysv, isrp, vpkf), *po; //0 - Г±ГІГ Г°Г»ГҐ, 1 - Г­Г®ГўГ»ГҐ Г§Г­Г Г·ГҐГ­ГЁГї //for (k=0; k<6; k++) { po=mu[k]; if (po) delete[]po; } if (mu) delete[]mu; 
 return 0;
 }
 void novNapMas()
 {
 	int j, k;
-	if ((!vysv) || (vysv==1)) {  //исходный или после повторных измерений
+	if ((!vysv) || (vysv==1)) {  //ГЁГ±ГµГ®Г¤Г­Г»Г© ГЁГ«ГЁ ГЇГ®Г±Г«ГҐ ГЇГ®ГўГІГ®Г°Г­Г»Гµ ГЁГ§Г¬ГҐГ°ГҐГ­ГЁГ©
 	if (!vyfv) { if (!vpmf) porver = por207; else if (vpmf==1) porver=por16035; }
 	else if (vyfv==1) { if (!vpkf) porver = poris84; else if (vpkf==1) porver=porin84; }
 	else if (vyfv==2) porver = poro16035; }
-	if (vysv==2) { //после обжига
+	if (vysv==2) { //ГЇГ®Г±Г«ГҐ Г®ГЎГ¦ГЁГЈГ 
 	if ((!vyfv) || (vyfv==2)) porver = poro16035;
 	else if (vyfv==1) porver = poro84; }
 }
@@ -81,18 +81,18 @@ snmov=new char[dsov]; sfnoov=new char[dsov]; szfov=new char[2*dsov];
 for (k=0; k<(2*dsov); k++) szfov[k]='\0';
 for (k=0; k<dsov; k++) { sndov[k]='\0'; ssdov[k]='\0'; szfovk[k]='\0'; snmov[k]='\0'; sfnoov[k]='\0'; }
 k=0; 
-sndov[k]='D'; k++; sndov[k]=':'; k++; sndov[k]='\\';k++; sndov[k]='\\'; k++; sndov[k]='_';  k++; sndov[k]='А';  k++;
-sndov[k]='с'; k++; sndov[k]='п'; k++; sndov[k]='и'; k++; sndov[k]='р';  k++; sndov[k]='а';  k++; sndov[k]='н';  k++;
-sndov[k]='т'; k++; sndov[k]='у'; k++; sndov[k]='р'; k++; sndov[k]='а';  k++; sndov[k]='\\'; k++; sndov[k]='\\'; k++;
+sndov[k]='D'; k++; sndov[k]=':'; k++; sndov[k]='\\';k++; sndov[k]='\\'; k++; sndov[k]='_';  k++; sndov[k]='ГЂ';  k++;
+sndov[k]='Г±'; k++; sndov[k]='ГЇ'; k++; sndov[k]='ГЁ'; k++; sndov[k]='Г°';  k++; sndov[k]='Г ';  k++; sndov[k]='Г­';  k++;
+sndov[k]='ГІ'; k++; sndov[k]='Гі'; k++; sndov[k]='Г°'; k++; sndov[k]='Г ';  k++; sndov[k]='\\'; k++; sndov[k]='\\'; k++;
 sndov[k]='t'; k++; sndov[k]='m'; k++; sndov[k]='p'; k++; sndov[k]='\\'; k++; sndov[k]='\\'; k++; sndov[k]='\0';
 k=0;
 ssdov[k]='C';  k++; ssdov[k]=':'; k++; ssdov[k]='\\'; k++; ssdov[k]='\\'; k++; ssdov[k]='U';  k++; ssdov[k]='s';  k++;
-ssdov[k]='e';  k++; ssdov[k]='r'; k++; ssdov[k]='s';  k++; ssdov[k]='\\'; k++; ssdov[k]='\\'; k++; ssdov[k]='А';  k++;
-ssdov[k]='н';  k++; ssdov[k]='д'; k++; ssdov[k]='р';  k++; ssdov[k]='е';  k++; ssdov[k]='й';  k++; ssdov[k]='\\'; k++;
+ssdov[k]='e';  k++; ssdov[k]='r'; k++; ssdov[k]='s';  k++; ssdov[k]='\\'; k++; ssdov[k]='\\'; k++; ssdov[k]='ГЂ';  k++;
+ssdov[k]='Г­';  k++; ssdov[k]='Г¤'; k++; ssdov[k]='Г°';  k++; ssdov[k]='ГҐ';  k++; ssdov[k]='Г©';  k++; ssdov[k]='\\'; k++;
 ssdov[k]='\\'; k++; ssdov[k]='D'; k++; ssdov[k]='o';  k++; ssdov[k]='c';  k++; ssdov[k]='u';  k++; ssdov[k]='m';  k++;
 ssdov[k]='e';  k++; ssdov[k]='n'; k++; ssdov[k]='t';  k++; ssdov[k]='s';  k++; ssdov[k]='\\'; k++; ssdov[k]='\\'; k++;
-ssdov[k]='_';  k++; ssdov[k]='А'; k++; ssdov[k]='с';  k++; ssdov[k]='п';  k++; ssdov[k]='и';  k++; ssdov[k]='р';  k++;
-ssdov[k]='а';  k++; ssdov[k]='н'; k++; ssdov[k]='т';  k++; ssdov[k]='у';  k++; ssdov[k]='р';  k++; ssdov[k]='а';  k++;
+ssdov[k]='_';  k++; ssdov[k]='ГЂ'; k++; ssdov[k]='Г±';  k++; ssdov[k]='ГЇ';  k++; ssdov[k]='ГЁ';  k++; ssdov[k]='Г°';  k++;
+ssdov[k]='Г ';  k++; ssdov[k]='Г­'; k++; ssdov[k]='ГІ';  k++; ssdov[k]='Гі';  k++; ssdov[k]='Г°';  k++; ssdov[k]='Г ';  k++;
 ssdov[k]='\\'; k++; ssdov[k]='\\';k++; ssdov[k]='t';  k++; ssdov[k]='m';  k++; ssdov[k]='p';  k++; ssdov[k]='\\'; k++;
 ssdov[k]='\\'; k++; ssdov[k]='\0';
 k=0;
@@ -295,14 +295,14 @@ if (fabs(wo)>eps)
 kuo[k]=(kmg*wmg+kal*wal+ksi*wsi)/wo; 
 else kuo[k]=0.0; } //for (k=0; k<n; k++) cout << "kuo = " << kuo[k] << "\ttere = " << tere[k] << endl;
 delete []mgo; delete []alo; delete []sio; return kuo; }
-double *oslaintestepcher(int dm, int vy, double *sc) //рассчитывает ослабление интегральной степени черноты для SiO2, Al2O3, MgO
+double *oslaintestepcher(int dm, int vy, double *sc) //Г°Г Г±Г±Г·ГЁГІГ»ГўГ ГҐГІ Г®Г±Г«Г ГЎГ«ГҐГ­ГЁГҐ ГЁГ­ГІГҐГЈГ°Г Г«ГјГ­Г®Г© Г±ГІГҐГЇГҐГ­ГЁ Г·ГҐГ°Г­Г®ГІГ» Г¤Г«Гї SiO2, Al2O3, MgO
 {	int n=2, k, j, w=0, p=6, q=10, m, l; double **scv=new double*[p], **hsv=new double*[p];
 	double *hs=new double[n], **kor=new double*[q], t0, t1, *vscs=new double[dm], *vscm=new double[dm], *vsca=new double[dm], *po=NULL;
 if ((!scv) || (!sc) || (!hsv) || (!hs) || (!kor)) { cout << "No memory" << endl; k=getchar(); exit(1);}
 	k=0;
 	sc[k]=1e0;   k++; sc[k]=0.94;  k++; sc[k]=0.87;  k++; sc[k]=0.801; k++; sc[k]=0.736; k++; 
 	sc[k]=0.676; k++; sc[k]=0.635; k++; sc[k]=0.590; k++; sc[k]=0.567; k++; sc[k]=0.543; k++; 
-	sc[k]=0.53;  k++; sc[k]=0.525; k++; sc[k]=0.515; k++; sc[k]=0.507; //степень черноты магнезита
+	sc[k]=0.53;  k++; sc[k]=0.525; k++; sc[k]=0.515; k++; sc[k]=0.507; //Г±ГІГҐГЇГҐГ­Гј Г·ГҐГ°Г­Г®ГІГ» Г¬Г ГЈГ­ГҐГ§ГЁГІГ 
 	w=0; scv[w]=sc; sc=new double[dm];
 	k=0;
 	hs[k]=98e-2; k++; hs[k]=2e-2;
@@ -310,71 +310,71 @@ if ((!scv) || (!sc) || (!hsv) || (!hs) || (!kor)) { cout << "No memory" << endl;
 	t0=hs[k]; k++; t1=hs[k];
 	k=0;
 	hs[k]=t0/(t0+t1); k++; hs[k]=t1/(t0+t1);
-	hsv[w]=hs; w++; hs=new double[n]; //Магнезит: 98 % - MgO, 2 % - SiO2
+	hsv[w]=hs; w++; hs=new double[n]; //ГЊГ ГЈГ­ГҐГ§ГЁГІ: 98 % - MgO, 2 % - SiO2
 	if ((!sc) || (!hs)) { cout << "No memory" << endl; k=getchar(); exit(1); }
 k=0;
 sc[k]=1e0;   k++; sc[k]=0.976; k++; sc[k]=0.949; k++; sc[k]=0.905; k++; sc[k]=0.859; k++; 
 sc[k]=0.812; k++; sc[k]=0.774; k++; sc[k]=0.737; k++; sc[k]=0.709; k++; sc[k]=0.681; k++; 
-sc[k]=0.661; k++; sc[k]=0.639; k++; sc[k]=0.626; k++; sc[k]=0.620; //степень черноты шамота
+sc[k]=0.661; k++; sc[k]=0.639; k++; sc[k]=0.626; k++; sc[k]=0.620; //Г±ГІГҐГЇГҐГ­Гј Г·ГҐГ°Г­Г®ГІГ» ГёГ Г¬Г®ГІГ 
 scv[w]=sc; sc=new double[dm]; 
 k=0; 
 hs[k]=56e-2; k++; hs[k]=396e-3; 
 k=0; 
 t0=hs[k]; k++; t1=hs[k]; 
 k=0; 
-hs[k]=t0/(t0+t1); k++; hs[k]=t1/(t0+t1); //Шамот: 56 % - SiO2, 39,6 % - Al2O3 - шамот, считаем, что магния нет в шамоте
+hs[k]=t0/(t0+t1); k++; hs[k]=t1/(t0+t1); //ГГ Г¬Г®ГІ: 56 % - SiO2, 39,6 % - Al2O3 - ГёГ Г¬Г®ГІ, Г±Г·ГЁГІГ ГҐГ¬, Г·ГІГ® Г¬Г ГЈГ­ГЁГї Г­ГҐГІ Гў ГёГ Г¬Г®ГІГҐ
 hsv[w]=hs; w++; hs=new double[n];
 if ((!sc) || (!hs)) { cout << "No memory" << endl; k=getchar(); exit(1);}
 k=0;
 sc[k]=1e0;    k++; sc[k]=98e-2;  k++; sc[k]=951e-3; k++; sc[k]=92e-2;  k++; sc[k]=883e-3; k++; 
 sc[k]=853e-3; k++; sc[k]=821e-3; k++; sc[k]=79e-2;  k++; sc[k]=767e-3; k++; sc[k]=746e-3; k++;
-sc[k]=73e-2;  k++; sc[k]=715e-3; k++; sc[k]=705e-3; k++; sc[k]=692e-3; //степень черноты корундошамота
+sc[k]=73e-2;  k++; sc[k]=715e-3; k++; sc[k]=705e-3; k++; sc[k]=692e-3; //Г±ГІГҐГЇГҐГ­Гј Г·ГҐГ°Г­Г®ГІГ» ГЄГ®Г°ГіГ­Г¤Г®ГёГ Г¬Г®ГІГ 
 scv[w]=sc; sc=new double[dm]; 
 k=0; 
 hs[k]=28e-2; k++; hs[k]=7e-1; 
 k=0; 
 t0=hs[k]; k++; t1=hs[k]; 
 k=0; 
-hs[k]=t0/(t0+t1); k++; hs[k]=t1/(t0+t1); //Корундошамот: 28 % - SiO2, 70 % - Al2O3 - корундошамот
+hs[k]=t0/(t0+t1); k++; hs[k]=t1/(t0+t1); //ГЉГ®Г°ГіГ­Г¤Г®ГёГ Г¬Г®ГІ: 28 % - SiO2, 70 % - Al2O3 - ГЄГ®Г°ГіГ­Г¤Г®ГёГ Г¬Г®ГІ
 hsv[w]=hs; w++; hs=new double[n];
 if ((!sc) || (!hs)) { cout << "No memory" << endl; k=getchar(); exit(1);}
 k=0;
 sc[k]=1.0000; k++; sc[k]=983e-3; k++; sc[k]=936e-3; k++; sc[k]=867e-3; k++; sc[k]=819e-3; k++; 
 sc[k]=721e-3; k++; sc[k]=659e-3; k++; sc[k]=593e-3; k++; sc[k]=541e-3; k++; sc[k]=49e-2;  k++; 
-sc[k]=453e-3; k++; sc[k]=429e-3; k++; sc[k]=403e-3; k++; sc[k]=384e-3; //степень черноты каолинового теплоизоляционного кирпича (КТК)
+sc[k]=453e-3; k++; sc[k]=429e-3; k++; sc[k]=403e-3; k++; sc[k]=384e-3; //Г±ГІГҐГЇГҐГ­Гј Г·ГҐГ°Г­Г®ГІГ» ГЄГ Г®Г«ГЁГ­Г®ГўГ®ГЈГ® ГІГҐГЇГ«Г®ГЁГ§Г®Г«ГїГ¶ГЁГ®Г­Г­Г®ГЈГ® ГЄГЁГ°ГЇГЁГ·Г  (ГЉГ’ГЉ)
 scv[w]=sc; sc=new double[dm];
 k=0; 
 hs[k]=57e-2; k++; hs[k]=4e-1; 
 k=0; 
 t0=hs[k]; k++; t1=hs[k]; 
 k=0; 
-hs[k]=t0/(t0+t1); k++; hs[k]=t1/(t0+t1); //КТК: 57 % - SiO2, 40 % - Al2O3
+hs[k]=t0/(t0+t1); k++; hs[k]=t1/(t0+t1); //ГЉГ’ГЉ: 57 % - SiO2, 40 % - Al2O3
 hsv[w]=hs; w++; hs=new double[n];
 if ((!sc) || (!hs)) { cout << "No memory" << endl; k=getchar(); exit(1); }
 k=0;
 sc[k]=1.000;  k++; sc[k]=984e-3; k++; sc[k]=941e-3; k++; sc[k]=882e-3; k++; sc[k]=813e-3; k++; 
 sc[k]=751e-3; k++; sc[k]=695e-3; k++; sc[k]=641e-3; k++; sc[k]=594e-3; k++; sc[k]=558e-3; k++; 
-sc[k]=53e-2;  k++; sc[k]=499e-3; k++; sc[k]=479e-3; k++; sc[k]=462e-3; //степень черноты муллита
+sc[k]=53e-2;  k++; sc[k]=499e-3; k++; sc[k]=479e-3; k++; sc[k]=462e-3; //Г±ГІГҐГЇГҐГ­Гј Г·ГҐГ°Г­Г®ГІГ» Г¬ГіГ«Г«ГЁГІГ 
 scv[w]=sc; sc=new double[dm];
 k=0; 
 hs[k]=28e-2; k++; hs[k]=72e-2; 
 k=0; 
 t0=hs[k]; k++; t1=hs[k]; 
 k=0; 
-hs[k]=t0/(t0+t1); k++; hs[k]=t1/(t0+t1); //Муллит: 28 % - SiO2, 72 % - Al2O3
+hs[k]=t0/(t0+t1); k++; hs[k]=t1/(t0+t1); //ГЊГіГ«Г«ГЁГІ: 28 % - SiO2, 72 % - Al2O3
 hsv[w]=hs; w++; hs=new double[n];
 if ((!sc) || (!hs)) { cout << "No memory" << endl; k=getchar(); exit(1);}
 k=0;
 sc[k]=1e0;    k++; sc[k]=984e-3; k++; sc[k]=953e-3; k++; sc[k]=917e-3; k++; sc[k]=854e-3; k++; 
 sc[k]=808e-3; k++; sc[k]=756e-3; k++; sc[k]=711e-3; k++; sc[k]=578e-3; k++; sc[k]=523e-3; k++; 
-sc[k]=495e-3; k++; sc[k]=468e-3; k++; sc[k]=448e-3; k++; sc[k]=429e-3; //степень черноты кремнезема
+sc[k]=495e-3; k++; sc[k]=468e-3; k++; sc[k]=448e-3; k++; sc[k]=429e-3; //Г±ГІГҐГЇГҐГ­Гј Г·ГҐГ°Г­Г®ГІГ» ГЄГ°ГҐГ¬Г­ГҐГ§ГҐГ¬Г 
 scv[w]=sc; 
 k=0; 
 hs[k]=985e-3; k++; hs[k]=1e-2; 
 k=0; 
 t0=hs[k]; k++; t1=hs[k]; 
 k=0; 
-hs[k]=t0/(t0+t1); k++; hs[k]=t1/(t0+t1); //Кремнезем: 98,5 % - SiO2, 1 % - Al2O3
+hs[k]=t0/(t0+t1); k++; hs[k]=t1/(t0+t1); //ГЉГ°ГҐГ¬Г­ГҐГ§ГҐГ¬: 98,5 % - SiO2, 1 % - Al2O3
 hsv[w]=hs; w++;
 for (j=0; j<dm; j++) { k=0; 
 for (m=1; m<p; m++)
@@ -438,30 +438,30 @@ mp=0.0; for (k=0; k<n; k++) { t=fabs(xp[k]-x[k]); if (t>mp) { mp=t; } }
 delete []pr; 
 return m; } 
 double vychopred(double  **mas, int m) {
-  double d=Determinant(mas, m); //Вычисление определителя
+  double d=Determinant(mas, m); //Г‚Г»Г·ГЁГ±Г«ГҐГ­ГЁГҐ Г®ГЇГ°ГҐГ¤ГҐГ«ГЁГІГҐГ«Гї
   return d; }
-double Determinant(double **mas, int m) { // Рекурсивное вычисление определителя
+double Determinant(double **mas, int m) { // ГђГҐГЄГіГ°Г±ГЁГўГ­Г®ГҐ ГўГ»Г·ГЁГ±Г«ГҐГ­ГЁГҐ Г®ГЇГ°ГҐГ¤ГҐГ«ГЁГІГҐГ«Гї
   int i;
   double **p, k=1., d=0.0, *po;
   p=new double*[m]; if (!p) { cout << snmv << endl; i=getchar(); exit(1); }
   for (i=0; i<m; i++) { po=new double[m]; if (!po) { cout << snmv << endl; i=getchar(); exit(1); } p[i]=po; }
   d=0;
-  if (m<1) cout << "Определитель вычислить невозможно!";
+  if (m<1) cout << "ГЋГЇГ°ГҐГ¤ГҐГ«ГЁГІГҐГ«Гј ГўГ»Г·ГЁГ±Г«ГЁГІГј Г­ГҐГўГ®Г§Г¬Г®Г¦Г­Г®!";
   if (m==1) { d = mas[0][0]; return (d); } 
   if (m==2) { d = mas[0][0]*mas[1][1]-mas[1][0]*mas[0][1]; return(d); } 
   if (m>2) {
 	for (i=0; i<m; i++) {
 		p=GetMatr(mas, p, i, 0, m); 
-		d=d+k*mas[i][0]*Determinant(p, m-1); //разложение по строкам по первому столбцу
-		k=-k; } } //(-1) в степени i
+		d=d+k*mas[i][0]*Determinant(p, m-1); //Г°Г Г§Г«Г®Г¦ГҐГ­ГЁГҐ ГЇГ® Г±ГІГ°Г®ГЄГ Г¬ ГЇГ® ГЇГҐГ°ГўГ®Г¬Гі Г±ГІГ®Г«ГЎГ¶Гі
+		k=-k; } } //(-1) Гў Г±ГІГҐГЇГҐГ­ГЁ i
 for (i=0; i<m; i++) { po=p[i]; delete []po; } return (d); }
-double **GetMatr(double **mas, double **p, int i, int j, int m) { //Получение матрицы без i-й строки и j-го столбца
+double **GetMatr(double **mas, double **p, int i, int j, int m) { //ГЏГ®Г«ГіГ·ГҐГ­ГЁГҐ Г¬Г ГІГ°ГЁГ¶Г» ГЎГҐГ§ i-Г© Г±ГІГ°Г®ГЄГЁ ГЁ j-ГЈГ® Г±ГІГ®Г«ГЎГ¶Г 
   int ki, kj, di, dj;
   di = 0;
-  for (ki = 0; ki<(m-1); ki++) { // проверка индекса строки
+  for (ki = 0; ki<(m-1); ki++) { // ГЇГ°Г®ГўГҐГ°ГЄГ  ГЁГ­Г¤ГҐГЄГ±Г  Г±ГІГ°Г®ГЄГЁ
     if (ki == i) di = 1;
     dj = 0;
-    for (kj = 0; kj<(m-1); kj++) { // проверка индекса столбца
+    for (kj = 0; kj<(m-1); kj++) { // ГЇГ°Г®ГўГҐГ°ГЄГ  ГЁГ­Г¤ГҐГЄГ±Г  Г±ГІГ®Г«ГЎГ¶Г 
       if (kj == j) dj = 1;
       p[ki][kj] = mas[ki + di][kj + dj];
 }  } return p; }
